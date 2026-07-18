a7b48c4477462ef7ee52445385e412f437fe3464
Zoom <xabou2010@gmail.com>
2026-07-17 21:07:52 -0600

    better logger

--- src/classes/Logger.ts
+++ src/classes/Logger.ts
@@ -1,5 +1,7 @@
 import pino from "pino";
 
+type LogData = object | undefined;
+
 export default class Logger {
   private logger = pino({
     level: process.env.LOG_LEVEL ?? "info",
@@ -17,23 +19,39 @@ export default class Logger {
         : undefined,
   });
 
-  info(message: string, data?: object) {
+  private parseArgs(
+    arg1: string | LogData,
+    arg2?: string | LogData,
+  ): [LogData, string?] {
+    if (typeof arg1 === "string") {
+      return [arg2 as LogData, arg1];
+    }
+
+    return [arg1, arg2 as string | undefined];
+  }
+
+  info(arg1: string | LogData, arg2?: string | LogData) {
+    const [data, message] = this.parseArgs(arg1, arg2);
     this.logger.info(data, message);
   }
 
-  warn(message: string, data?: object) {
+  warn(arg1: string | LogData, arg2?: string | LogData) {
+    const [data, message] = this.parseArgs(arg1, arg2);
     this.logger.warn(data, message);
   }
 
-  error(message: string, data?: object) {
+  error(arg1: string | LogData, arg2?: string | LogData) {
+    const [data, message] = this.parseArgs(arg1, arg2);
     this.logger.error(data, message);
   }
 
-  debug(message: string, data?: object) {
+  debug(arg1: string | LogData, arg2?: string | LogData) {
+    const [data, message] = this.parseArgs(arg1, arg2);
     this.logger.debug(data, message);
   }
 
-  fatal(message: string, data?: object) {
+  fatal(arg1: string | LogData, arg2?: string | LogData) {
+    const [data, message] = this.parseArgs(arg1, arg2);
     this.logger.fatal(data, message);
   }
 }
